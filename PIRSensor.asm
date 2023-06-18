c9608e6ecf33b74dc52d78c8e4cb98f940564e35
Osman Şeyban <122537180+oseyban@users.noreply.github.com>
2023-06-18 12:28:54 +0300

    Add files via upload

---
+++ PIRSensor.asm
@@ -0,0 +1,7 @@
+_main:
+;PIRSensor.c,1 :: 		void main() {
+;PIRSensor.c,3 :: 		}
+L_end_main:
+L__main_end_loop:
+B	L__main_end_loop
+; end of _main
---
+++ PIRSensor.cp
@@ -0,0 +1,4 @@
+#line 1 "C:/Users/Public/Documents/Mikroelektronika/mikroC PRO for ARM/Examples/PIRSensor.c"
+void main() {
+
+}
